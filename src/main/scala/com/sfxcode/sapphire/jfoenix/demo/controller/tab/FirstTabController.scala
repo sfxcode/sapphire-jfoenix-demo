018586aa245a40f202059ca43ecae9b200a2f5be
sfxcode <tom@sfxcode.com>
2018-07-25 20:33:26 +0200

    small UI Updates

--- src/main/scala/com/sfxcode/sapphire/jfoenix/demo/controller/tab/FirstTabController.scala
+++ src/main/scala/com/sfxcode/sapphire/jfoenix/demo/controller/tab/FirstTabController.scala
@@ -1,12 +1,26 @@
 package com.sfxcode.sapphire.jfoenix.demo.controller.tab
 
 import com.sfxcode.sapphire.jfoenix.demo.controller.base.AbstractViewController
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.scene.control.{Label, TextField}
 
 class FirstTabController extends AbstractViewController {
+
+
+  @FXML var infoLabel: Label = _
+  @FXML var inputTextField: TextField = _
 
   override def didGainVisibilityFirstTime() {
   }
 
+  def actionUpdateLabel(event: ActionEvent): Unit = {
+    if (inputTextField.getText.isEmpty)
+      infoLabel.setText("Empty Input")
+    else
+      infoLabel.setText(inputTextField.getText)
+    inputTextField.clear()
+  }
 
 }
 
